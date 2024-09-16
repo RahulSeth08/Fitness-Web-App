b62b0d5aa1112c393414830f0521c298ffb3a593
Gourav Kumar <raj11gourav44@gmail.com>
2024-09-16 23:40:25 +0530

    Initialization completed

---
+++ BACKEND/GottaFitGuys (1)/GottaFitGuys/src/main/java/com/webapp/GottaFitGuys/GottaFitGuysApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.GottaFitGuys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GottaFitGuysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GottaFitGuysApplication.class, args);
+	}
+
+}
---
+++ BACKEND/GottaFitGuys (1)/GottaFitGuys/src/test/java/com/webapp/GottaFitGuys/GottaFitGuysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.GottaFitGuys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GottaFitGuysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
