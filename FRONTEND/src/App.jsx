c5ec652653bf936d0ef6c99d6de3433303b864b9
Rahul Seth <sethrahul4u@gmail.com>
2024-09-17 00:08:08 +0530

    Front-End Initialized

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="bg-red-300 flex justify-center items-center text-4xl h-[100vh] font-mono font-bold">
+      Hello world!
+    </h1>
+  );
+}
