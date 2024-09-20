5481551e91be0b71560bff51b0b3922a8d1f051e
Gourav Kumar <raj11gourav44@gmail.com>
2024-09-21 01:05:47 +0530

    Added Security config for csrf handling

---
+++ BACKEND/src/main/java/com/webapp/FitGuysWeb/config/Securityconfig.java
@@ -0,0 +1,2 @@
+package com.webapp.FitGuysWeb.config;public class Securityconfig {
+}
--- BACKEND/src/main/java/com/webapp/FitGuysWeb/controller/userController.java
+++ BACKEND/src/main/java/com/webapp/FitGuysWeb/controller/userController.java
@@ -2,12 +2,9 @@ package com.webapp.FitGuysWeb.controller;
 
 import com.webapp.FitGuysWeb.model.user;
 import com.webapp.FitGuysWeb.service.userService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,11 +14,12 @@ import java.util.List;
 public class userController {
     @Autowired
     private userService Service;
-    @GetMapping("/")
-    public String getHomePage(HttpServletRequest request) {
-        return "Hello"+ request.getSession().getId();
+    @PostMapping("/user")
+    user newUser(@RequestBody user newUser) {
+        return Service.saveUser(newUser);
     }
-    @GetMapping("/user")
+
+    @GetMapping("/users")
     public List<user> getUser() {
         return Service.getUser();
     }
