0c0ae9cf39846b7a912e8ddeffc1d7071a427214
Darshan Choudhary <97291483+DarshanchGIT@users.noreply.github.com>
2024-09-24 14:12:51 +0530

    Fixed navigation and responsivness issues

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,8 +1,10 @@
 import { Landing } from "./components/Landing";
+import { Navbar } from "./components/landingComp/Navbar";
 
 export default function App() {
   return (
     <>
+      <Navbar/>
       <Landing />
     </>
   );
