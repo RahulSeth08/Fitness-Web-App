9b21a3d367c596f9d134666ae9d24e9503b34f4c
Rahul Seth <sethrahul4u@gmail.com>
2024-09-26 00:34:48 +0530

    Added Registeration Page

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,9 +1,11 @@
 import { Landing } from "./components/Landing";
 import { Navbar } from "./components/landingComp/Navbar";
+import { Register } from "./components/landingComp/Register";
 
 export default function App() {
   return (
     <>
+      <Register/>
       <Navbar/>
       <Landing />
     </>
--- FRONTEND/src/components/Landing.jsx
+++ FRONTEND/src/components/Landing.jsx
@@ -4,12 +4,16 @@ import { Services } from "./landingComp/Services";
 import { Nutrition } from "./landingComp/Nutrition";
 // import { About } from "./landingComp/About";
 import { Footer } from "./landingComp/Footer";
+import { Register } from "./landingComp/Register";
 
 export function Landing() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black text-gray-100">
       {/* <Navbar /> */}
       <main>
+        {/* <section id="register">
+          <Register /> */}
+        {/* </section> */}
         <section id="hero">
           <Hero />
         </section>
