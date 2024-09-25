cfa1b8338da7f57f07aa0b5e63469953b152c86c
Rahul Seth <sethrahul4u@gmail.com>
2024-09-26 01:02:57 +0530

    Added Register Page

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,11 +1,9 @@
 import { Landing } from "./components/Landing";
 import { Navbar } from "./components/landingComp/Navbar";
-import { Register } from "./components/landingComp/Register";
 
 export default function App() {
   return (
     <>
-      <Register/>
       <Navbar/>
       <Landing />
     </>
