18ce8201de55cd03535c61e25ad8406cc08af1ff
Madhav <ms99194@gmail.com>
2024-09-28 02:17:54 +0530

    Created Blog Component

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -3,13 +3,19 @@ import { Navbar } from "./components/landingComp/Navbar";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { SignIn } from "./components/auth/SignIn";
 import { SignUp } from "./components/auth/SignUp";
+import {Blog} from "./components/pages/Blog";
+import {Podcasts} from "./components/pages/Podcasts";
+
 
 function AppContent() {
   // Get the current location
   const location = useLocation();
+  
+
 
   // Check if the current path is signup or signin
-  const isAuthRoute = location.pathname === "/auth/signup" || location.pathname === "/auth/signin";
+  const isAuthRoute = location.pathname === "/auth/signup" || location.pathname === "/auth/signin" || location.pathname === "/blog";
+ 
 
   return (
     <>
@@ -19,6 +25,8 @@ function AppContent() {
         <Route path="/" element={<Landing />} />
         <Route path="/auth/signup" element={<SignUp />} />
         <Route path="/auth/signin" element={<SignIn />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/podcasts" element={<Podcasts />} />
       </Routes>
     </>
   );
---
+++ FRONTEND/src/components/pages/Podcasts.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Podcasts = () => {
+  return <div className="flex items-center justify-center h-[100vh]">Podcasts</div>;
+};
